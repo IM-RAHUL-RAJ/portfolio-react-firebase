6c032cc1ad5a34ba9513f3e85e6d0c4a9581d17d
Rahul Raj <rahuldps3313@gmail.com>
2021-09-04 15:16:33 +0530

    about us,contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './Navbar';
 import "./index.css";
 import Header from './Header';
 import OurWork from './OurWork';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Footer from './Footer';
 
 const App=()=>{
   return (
@@ -10,6 +14,11 @@ const App=()=>{
   <Navbar/>
   <Header/>
   <OurWork/>
+  <About/>
+  <Services/>
+  <Contact/>
+  \<Footer/>
+
   </>
   )
 }
